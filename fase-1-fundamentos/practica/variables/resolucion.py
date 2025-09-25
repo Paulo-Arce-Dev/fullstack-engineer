33c9b946745ab1251e112243d23e5b10ca68c82f
Paulo Arce <codebypaulo@gmail.com>
2025-09-25 01:23:17 -0300

    resolviendo el ejercicio 10

--- fase-1-fundamentos/practica/variables/resolucion.py
+++ fase-1-fundamentos/practica/variables/resolucion.py
@@ -1,15 +1,21 @@
 # Resolución de ejercicios - Variables
 
 # Ejercicio 1 - Primera Variable
+print("----- Ejercicio 1 -----")
+
 nombre = 'Paulo'
 print(nombre)
 
 # Ejercicio 2 - Operaciones Simples
+print("----- Ejercicio 2 -----")
+
 a = 10
 b = 3
 resultado = a + b 
 
 # Ejercicios 3 - Intercambio de Variables
+print("----- Ejercicio 3 -----")
+
 x = 5
 y = 8
 
@@ -18,12 +24,16 @@ y = x
 print(x, y)
 
 # Ejercicio 4 - Información Personal
+print("----- Ejercicio 4 -----")
+
 edad = 24
 ciudad_de_nacimiento = "Villa Maria"
 color_favorito = 'Amarillo'
 print(f"Tengo {edad} de edad, naci en la ciudad {ciudad_de_nacimiento} y mi color favorito es el {color_favorito}")
 
 # Ejercicio 5 - Cálculo de Área
+print("----- Ejercicio 5 -----")
+
 largo_rectangulo_cm = 16
 ancho_rectangulo_cm = 9
 
@@ -34,7 +44,7 @@ print(f"El area del rectangulo es de: {area_rectangulo}")
 print(f"El perimetro del rectangulo es de: {perimetro_rectangulo}")
 
 # Ejercicio 6 - Conversión de Temperatura
-
+print("----- Ejercicio 6 -----")
 # 1 °C es igaual a 33.8 Fahrenheit
 # 1 °C es igaual a 274.15 Kelvin
 
@@ -47,6 +57,8 @@ print(f"Temperatura de Celcius a Fahrenheit: {conversion_a_fahrenheit} F")
 print(f"Temperatura de Celcius a Kelvin: {conversion_a_kelvin} K")
 
 # Ejercicio 7 - Datos del Estudiante
+print("----- Ejercicio 7 -----")
+
 nombre_estudiante = 'Guido'
 edad_estudiante = 17
 carrera_estudiante = 'Tecnicatura superior en programacion'
@@ -62,6 +74,8 @@ print(f"Estudia la carrera: {carrera_estudiante} y mi promedio es de: {promedio}
 print(f"Estado del estudiante: {aprobado}")
 
 # Ejercicio 8 - Calculadora de Descuento
+print("----- Ejercicio 8 -----")
+
 precio_producto = 80000
 print(f"El precio del producto es de: ${precio_producto} ARS")
 
@@ -77,9 +91,10 @@ dinero_ahorrado = precio_producto - precio_final
 print(f"El dinero ahorrado es de: ${dinero_ahorrado} ARS")
 
 # Ejercicio 9 - Tiempo de Vida
+print("----- Ejercicio 9 -----")
 
 edad_actual = 24
-print(f"Tu edad actual es de: edad_actual")
+print(f"Tu edad actual es de: {edad_actual}")
  
 dias_vividos = edad_actual * 365
 print(f"Dias vividos aproximadamente: {dias_vividos}")
@@ -89,3 +104,20 @@ print(f"Semanas vividas aproximadamente: {semanas_vividas}")
 
 años_para_llegar_80 = 80 - edad_actual
 print(f"Para llegar a tener 80 años me faltan de vivir {años_para_llegar_80}")
+
+# Ejercicio 10 - Análisis de Texto
+print("----- Ejercicio 10 -----")
+
+frase = "todo en la vida es temporal"
+
+# frase con espacios
+caracteres_de_frase = len(frase)
+print(f"Caracteres de la frase: {caracteres_de_frase}")
+
+# frase sin espacios (eliminando espacios)
+caracteres_sin_espacios_frase = len(frase.replace(" ", ""))
+print(f"Caracteres sin espacios en la frase: {caracteres_sin_espacios_frase}")
+
+# contando las palabras
+cantidad_palabras_frase = len(frase.split(" "))
+print(f"Cantidad de palabras de la frase: {cantidad_palabras_frase}")
