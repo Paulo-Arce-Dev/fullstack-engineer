f9813762a7b14bab4652e681c0cb3a7319b9cf38
Paulo Arce <codebypaulo@gmail.com>
2025-09-26 16:31:13 -0300

    agregando ejercicios de logica

---
+++ fase-1-fundamentos/practica/logica/soluciones.py
@@ -0,0 +1,56 @@
+# Guia Ejercicios 1 - Lógica de programación
+
+# Ejercicio 2
+""" 
+numero_usuario = int(input("Ingrese un numero por favor "))
+numero_usuario_cubo = (numero_usuario * numero_usuario) * numero_usuario
+print(f"El numero del usuario elevado al cubo es: {numero_usuario_cubo}")
+"""
+
+# Ejercicio 3
+""" 
+ano_actual = int(input("Ingrese el año actual por favor: "))
+ano_nacimiento = int(input("Ingrese el año de su nacimiento por favor: "))
+edad_usuario = ano_actual - ano_nacimiento
+print(f"Su edad es de: {edad_usuario} anos")
+"""
+
+# Ejercicio 4
+"""  
+distancia_ciudades = float(input("Ingresa la distancia en KM que hay entre dos ciudades: "))
+velocidad_promedio_vehiculo = float(input("Ingrese la velocidad promedio del vehiculo en el que viaja: "))
+tiempo_aproximado_llegada = distancia_ciudades / velocidad_promedio_vehiculo
+print(f"El tiempo aproximado de llegada es de: {tiempo_aproximado_llegada:.2f} horas")
+"""
+
+# Ejercicio 5
+"""  
+sueldo_fijo = 15000
+comision = 5 / 100
+
+total_facturado = float(input("Ingrese el total facturado por favor: "))
+comision_aplicada = total_facturado * comision
+
+sueldo_total = sueldo_fijo + comision_aplicada
+print(f"El sueldo total a pagar es de: {sueldo_total} ARS")
+"""
+
+# Ejercicio 6
+"""
+primer_nota = int(input("Ingrese la nota de su primer examen: "))
+segunda_nota = int(input("Ingrese la nota de su segundo examen: "))
+tercera_nota = int(input("Ingrese la nota de su tercer examen: "))
+
+promeido = (primer_nota + segunda_nota + tercera_nota) / 3 
+print(f"Su promedio es de: {promeido:.2f}")
+""" 
+
+# Ejercicio 7
+""" """
+metros_cuadrados_totales = float(input("Ingresa los metros cuadrados de un predio: "))
+
+
+
+# Ejercicio 8
+# Ejercicio 9
+# Ejercicio 10
