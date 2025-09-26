1df243cd0a8b8d56f0ab47831db1133dab9fbb6e
Paulo Arce <codebypaulo@gmail.com>
2025-09-26 17:49:27 -0300

    resolviendo el ejercicio 13

--- fase-1-fundamentos/practica/variables/resolucion.py
+++ fase-1-fundamentos/practica/variables/resolucion.py
@@ -1,10 +1,12 @@
 # Resolución de ejercicios - Variables
 
+
 # Ejercicio 1 - Primera Variable
 print("----- Ejercicio 1 -----")
 
 nombre = 'Paulo'
 print(nombre)
+
 
 # Ejercicio 2 - Operaciones Simples
 print("----- Ejercicio 2 -----")
@@ -12,6 +14,7 @@ print("----- Ejercicio 2 -----")
 a = 10
 b = 3
 resultado = a + b 
+
 
 # Ejercicios 3 - Intercambio de Variables
 print("----- Ejercicio 3 -----")
@@ -23,6 +26,7 @@ x = y
 y = x 
 print(x, y)
 
+
 # Ejercicio 4 - Información Personal
 print("----- Ejercicio 4 -----")
 
@@ -30,6 +34,7 @@ edad = 24
 ciudad_de_nacimiento = "Villa Maria"
 color_favorito = 'Amarillo'
 print(f"Tengo {edad} de edad, naci en la ciudad {ciudad_de_nacimiento} y mi color favorito es el {color_favorito}")
+
 
 # Ejercicio 5 - Cálculo de Área
 print("----- Ejercicio 5 -----")
@@ -43,6 +48,7 @@ perimetro_rectangulo = (largo_rectangulo_cm * 2) + (ancho_rectangulo_cm * 2)
 print(f"El area del rectangulo es de: {area_rectangulo}")
 print(f"El perimetro del rectangulo es de: {perimetro_rectangulo}")
 
+
 # Ejercicio 6 - Conversión de Temperatura
 print("----- Ejercicio 6 -----")
 # 1 °C es igaual a 33.8 Fahrenheit
@@ -55,6 +61,7 @@ conversion_a_kelvin = temperatura_celcius * 274.15
 print(f"Temperatura actual en Celcius: {temperatura_celcius} C")
 print(f"Temperatura de Celcius a Fahrenheit: {conversion_a_fahrenheit} F")
 print(f"Temperatura de Celcius a Kelvin: {conversion_a_kelvin} K")
+
 
 # Ejercicio 7 - Datos del Estudiante
 print("----- Ejercicio 7 -----")
@@ -73,6 +80,7 @@ print(f"Soy {nombre_estudiante}, tengo {edad_estudiante} años")
 print(f"Estudia la carrera: {carrera_estudiante} y mi promedio es de: {promedio}")
 print(f"Estado del estudiante: {aprobado}")
 
+
 # Ejercicio 8 - Calculadora de Descuento
 print("----- Ejercicio 8 -----")
 
@@ -90,6 +98,7 @@ print(f"El precio final a pagar es de: ${precio_final} ARS")
 dinero_ahorrado = precio_producto - precio_final
 print(f"El dinero ahorrado es de: ${dinero_ahorrado} ARS")
 
+
 # Ejercicio 9 - Tiempo de Vida
 print("----- Ejercicio 9 -----")
 
@@ -104,6 +113,7 @@ print(f"Semanas vividas aproximadamente: {semanas_vividas}")
 
 años_para_llegar_80 = 80 - edad_actual
 print(f"Para llegar a tener 80 años me faltan de vivir {años_para_llegar_80}")
+
 
 # Ejercicio 10 - Análisis de Texto
 print("----- Ejercicio 10 -----")
@@ -121,6 +131,7 @@ print(f"Caracteres sin espacios en la frase: {caracteres_sin_espacios_frase}")
 # contando las palabras
 cantidad_palabras_frase = len(frase.split(" "))
 print(f"Cantidad de palabras de la frase: {cantidad_palabras_frase}")
+
 
 # Ejercicio 11 - Sistema de Puntuación
 print("----- Ejercicio 11 -----")
@@ -146,6 +157,7 @@ total_niveles_completados = cantidad_veces_completado_lvl_1 + cantidad_veces_com
 nivel_promedio = suma_valores_niveles / total_niveles_completados
 print(f"El promedio de nivel del jugador es de: {nivel_promedio:.2f} aproximadamente.")
 
+
 # Ejercicio 12 - Gestor de Presupuesto
 print("----- Ejercicio 12 -----")
 
@@ -167,4 +179,16 @@ print(f"El porcentaje de gastos respecto a mi salario es de: {porcentaje_gastos_
 if porcentaje_gastos_sobre_salario > 80:
   print(f"Estas gastando mas del 80% de tu salario!")
 else:
-  print(f"No estas gastando mas del 80% de tu salario. Genial, puedes ahorrar dinero!") 
+  print(f"No estas gastando mas del 80% de tu salario. Genial, puedes ahorrar dinero!")
+
+
+# Ejercicio 13 - Conversión de Unidades
+print("----- Ejercicio 13 -----")
+
+tiempo_de_carrera_minutos = 1.48
+
+carrera_en_segundos = tiempo_de_carrera_minutos * 60
+print(f"El tiempo de carrera en segundos es de: {carrera_en_segundos:.2f}s")
+
+carrera_en_horas = tiempo_de_carrera_minutos / 60
+print(f"El tiempo de carrera en horas es de: {carrera_en_horas:.2f}h")
